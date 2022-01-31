f3f7281dc41ae122a4561f1ef0ec722f20537c77
Vikram Singh Boran <66003221+VikramSinghBoran@users.noreply.github.com>
2022-01-31 13:07:41 +0530

    Update circle.js
    made circle.js
    overriding done for method 'calculateArea'.

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,4 +1,17 @@
 // import using require
+const Shape = require('./shape');
+
+module.exports = class Circle extends Shape {
+                  super(radius){
+                    this.radius= radius;
+                  }
+
+                  calculateArea(){
+                  console.log('Area');
+                  }
+  
+                }
+
 
 // declare class
 
