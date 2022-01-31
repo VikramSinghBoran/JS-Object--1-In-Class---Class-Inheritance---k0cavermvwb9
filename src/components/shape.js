7e7f37b759fdb5fbfe968dd81e41b4bc9a97d81b
Vikram Singh Boran <66003221+VikramSinghBoran@users.noreply.github.com>
2022-01-31 13:02:17 +0530

    Update shape.js
    created shape class. exported it.

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
 // declare class
-
+module.exports = class Shape {
+  constructor(color){
+    this.color = color ;
+  }
+  function drawShape(){
+  }
+  
+  function calculateArea(){
+  }
+  
+}
 // export class using module.exports
